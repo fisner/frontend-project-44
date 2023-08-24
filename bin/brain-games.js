d7fa143a5b9d603314efcd9672193a174103cf09
fisner <fisner@LAPTOP-T2EUC6TP>
2023-08-24 12:00:01 +0300

    9

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!')
-import { whatName } from "../src/cli"
+import { whatName } from '../src/cli';
 
+console.log('Welcome to the Brain Games!');
